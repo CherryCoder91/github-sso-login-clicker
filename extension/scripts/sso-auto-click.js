7998587aef8b36d7aa3d4e412e4faa845abf6d36
CherryCoder91 <jamespickup2@gmail.com>
2023-08-31 11:43:16 +0100

    Update to support non specific sso login links

--- extension/scripts/sso-auto-click.js
+++ extension/scripts/sso-auto-click.js
@@ -1,22 +1,22 @@
 // If the sign in link appears on a page/pr list page  automate the click.
 // (Not currently locale agnostic)
 var links = document.getElementsByTagName('A');
-for (var i=0; i < links.length; i++) {
+for (var i = 0; i < links.length; i++) {
     if (links[i].textContent.indexOf('Single sign-on') > -1) {
-        links[i].click();  
-        break;  
+        links[i].click();
+        break;
     }
 }
 
 // If the user is presented with the click 'continue' to log in page automate the click.
+// (Not currently locale agnostic)
 var url = window.location.toString()
-if (url.indexOf('github.com/enterprises/')
-    > -1 && url.indexOf('/sso') > -1) {
 
-    var buttons = document.getElementsByTagName('button');
+var buttons = document.getElementsByTagName('button');
 
-    for (var i = 0; i < buttons.length; i++) {
-        if (buttons[i].classList.contains('btn-primary') && buttons[i].classList.contains('btn') && buttons[i].classList.contains('btn-block')) {
+for (var i = 0; i < buttons.length; i++) {
+    if (buttons[i].classList.contains('btn-primary') && buttons[i].classList.contains('btn') && buttons[i].classList.contains('btn-block')) {
+        if (buttons[i].textContent.contains('Continue')) {
             buttons[i].click();
             break;
         }
